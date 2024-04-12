627042c76a43515c8cdce36053290ed625b0cad7
CHARLLYS97 <jhonnycharllys@hotmail.com>
2024-04-11 22:37:24 -0300

    Implementando a função buscar cidade

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
-let clique= "E,ai já clicou no botão"
+let chave = "6b46ba426bf834bcbe70a4ee3a7fdc5a"
 
-function cliqueiNoBotao(){
+
+async function buscarCidade() {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=londres&appid=6b46ba426bf834bcbe70a4ee3a7fdc5a").then(resposta => resposta.json())
+
+    // AWAIT = ESPERE 
+    // FETCH -> FERRAMENTA DO JAVASCRIPT PARA ACESSAR  SERVIDORES
+    // THEN -> ENTÃO
+    /// JSON -> JAVASCRIPT OBJECT NOTATION ( O FORMATO QUE O JAVASCRIPT ENTENDE)
+
+    console.log(dados)
+
+}
+
+function cliqueiNoBotao() {
     let cidade = document.querySelector(".input-cidade").value
 
 
-console.log(cidade)
+    buscarCidade()
 
 }
+
